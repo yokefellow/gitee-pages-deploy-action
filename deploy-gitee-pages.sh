753e2fadf1e249cf2ba0536e24670c5432ae86e3
yokefellow <yuan1241900094@gmail.com>
2020-07-19 22:27:54 +0800

    update

--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-GIT_SSH_COMMAND="ssh -v"
-echo "INPUT_GITEE_REPO=$INPUT_GITEE_REPO"
+git config --global user.email "yuan1241900094@qq.com"
+git config --global user.name "yokefellow"
 git remote set-url --push origin "$INPUT_GITEE_REPO"
 git commit -m "$INPUT_COMMIT_MESSAGE"
 git push --all origin 
