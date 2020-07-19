fcfc82e6cc32c506a800f0cc4fc35bcff398fcb7
yokefellow <yuan1241900094@gmail.com>
2020-07-19 22:34:12 +0800

    update

--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -5,5 +5,7 @@ set -e
 git config --global user.email "yuan1241900094@qq.com"
 git config --global user.name "yokefellow"
 git remote set-url --push origin "$INPUT_GITEE_REPO"
+git init
+git add -A
 git commit -m "$INPUT_COMMIT_MESSAGE"
 git push --all origin 
