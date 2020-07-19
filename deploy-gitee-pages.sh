cd0e72c6517da29ea8e6a6d11c735aede41c791f
yokefellow <yuan1241900094@gmail.com>
2020-07-19 21:57:32 +0800

    update

--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -3,7 +3,7 @@
 set -e
 
 GIT_SSH_COMMAND="ssh -v"
-echo $INPUT_GITEE_REPO
-git remote set-url --push origin $INPUT_GITEE_REPO
-git commit -m $INPUT_COMMIT_MESSAGE
+echo "INPUT_GITEE_REPO=$INPUT_GITEE_REPO"
+git remote set-url --push origin "$INPUT_GITEE_REPO"
+git commit -m "$INPUT_COMMIT_MESSAGE"
 git push --all origin 
