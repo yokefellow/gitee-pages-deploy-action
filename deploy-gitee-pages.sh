b15f6db33af544d22bfb5e5d1d6b54b8c7aec3df
yokefellow <yuan1241900094@gmail.com>
2020-07-21 01:38:02 +0800

    update

--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -18,9 +18,6 @@ git commit -m "$INPUT_COMMIT_MESSAGE"
 git remote rm origin
 git remote add origin $INPUT_GITEE_REPO
 
-git remote -v
-git branch -a
-
-git pull $INPUT_SOURCE_REMOTE_BRANCH master --allow-unrelated-histories
-git branch --set-upstream-to=origin/$INPUT_SOURCE_REMOTE_BRANCH $INPUT_SOURCE_LOCAL_BRANCH
+git pull origin $INPUT_SOURCE_GITEE_REMOTE_BRANCH --allow-unrelated-histories
+git branch --set-upstream-to=origin/$INPUT_SOURCE_GITEE_REMOTE_BRANCH $INPUT_SOURCE_GITHUB_REMOTE_BRANCH
 git push                                                      
