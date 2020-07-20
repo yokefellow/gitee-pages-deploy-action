cd7230d08e55679f8b0201fb0bfa7eb85686328a
yokefellow <yuan1241900094@gmail.com>
2020-07-20 15:33:40 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
+# Container image that runs your code
 FROM ubuntu
 
 RUN apt-get update \
   && apt-get install -y git openssh-client \
   && echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
-ADD *.sh /
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY *.sh /
 RUN chmod +x /*.sh
 
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-git config --global user.email "1241900094@qq.com"
-git config --global user.name "yokefellow"
-
 cd "$INPUT_FOLDER"
 git init
 git add -A
@@ -14,6 +11,10 @@ git push -f "$INPUT_GITEE_REPO" master
 cd ..
 rm -rf .git
 git init
+
+git config user.email "$INPUT_GIT_USER_EMAIL"
+git config user.name "$INPUT_GIT_USER_NAME"
+
 git add -A
 git commit -m "$INPUT_COMMIT_MESSAGE"
-git push -f "$INPUT_GITEE_REPO" master:gh-pages-source
+git push -f "$INPUT_GITEE_REPO" master:"$INPUT_SOURCE_BRANCH"
