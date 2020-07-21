5c797022eed4225d06c5f7da2634b5c49bd41a7f
yokefellow <yuan1241900094@gmail.com>
2020-07-21 22:06:48 +0800

    update

--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -5,15 +5,16 @@ set -e
 git config --global user.email "$INPUT_GIT_USER_EMAIL"
 git config --global user.name "$INPUT_GIT_USER_NAME"
 
+
 cd "$INPUT_FOLDER"
 git init
 git add -A
 git commit -m "$INPUT_COMMIT_MESSAGE"
 git push -f "$INPUT_GITEE_REPO" master
 
-cd ..
+# cd ..
 
-git remote rm origin
-git remote add origin $INPUT_GITEE_REPO
+# git remote rm origin
+# git remote add origin $INPUT_GITEE_REPO
 
-git push -f origin $INPUT_SOURCE_GITHUB_REMOTE_BRANCH:$INPUT_SOURCE_GITEE_REMOTE_BRANCH
+# git push -f origin $INPUT_SOURCE_GITHUB_REMOTE_BRANCH:$INPUT_SOURCE_GITEE_REMOTE_BRANCH
