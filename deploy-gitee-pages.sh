63df728b20a77d995f673b24228aad525281d305
yokefellow <yuan1241900094@gmail.com>
2020-07-22 00:38:13 +0800

    update

--- deploy-gitee-pages.sh
+++ deploy-gitee-pages.sh
@@ -6,7 +6,7 @@ git config --global user.email "$INPUT_GIT_USER_EMAIL"
 git config --global user.name "$INPUT_GIT_USER_NAME"
 
 mkdir tmp
-git clone --depth=1 --single-branch --branch "$INPUT_GITEE_BRANCH" "$INPUT_GITEE_REPO" ./tmp
+git clone --depth=1 --single-branch --branch "$INPUT_GITEE_PAGES_BRANCH" "$INPUT_GITEE_REPO" ./tmp
 cd ./tmp
 git rm -r --ignore-unmatch *
 cp -r ../"$INPUT_FOLDER"/* ./
